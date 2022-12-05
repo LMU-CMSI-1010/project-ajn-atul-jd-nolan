f545934d99109e8979cd06c67de47f7e956aba80
avenky72 <109006663+avenky72@users.noreply.github.com>
2022-12-04 20:57:28 -0800

    Update background2.py

--- background2.py
+++ background2.py
@@ -1,6 +1,10 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
 
 screen_width = 1000
 screen_height = 1000
@@ -9,20 +13,19 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 # define game variables
 tile_size = 50
+game_over = 0
 main_menu = True
 
 # load images
-sn_img = pygame.image.load('gamegraphics/moon.png')
-sun_img = pygame.transform.scale(sn_img, (50,50))
 b_img = pygame.image.load('gamegraphics/sky.png')
-bg_img = pygame.transform.scale(b_img, (1000,1000))
-
-# start and end image
+bg_img = pygame.transform.scale(b_img, (1000, 1000))
+sn_img = pygame.image.load('gamegraphics/moon.png')
+sun_img = pygame.transform.scale(sn_img, (50, 50))
 start_img = pygame.image.load('gamegraphics/start button.png')
 exit_img = pygame.image.load('gamegraphics/exit button.png')
 
 
-class Buttons:
+class Button():
     def __init__(self, x, y, image):
         self.image = image
         self.rect = self.image.get_rect()
@@ -30,46 +33,35 @@ class Buttons:
         self.rect.y = y
         self.clicked = False
 
-        # def draw(self):
-        # action = False
-        # get button to click
-        posx, posy = pygame.mouse.get_pos()
-        # if mouse over button
-        # FIND A WAY TO CLICK START AND EXIT BUTTONS THROUGH MOUSE HOVER OVER THE BUTTON
+    def draw(self):
+        action = False
 
+        # get mouse position
+        pos = pygame.mouse.get_pos()
 
-"""
-        if posx > 0 and posx < 500:
+        # check mouseover and clicked conditions
+        if self.rect.colliderect(pos):
             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 action = True
                 self.clicked = True
-            if pygame.mouse.get_pressed()[0] == 0:
-                self.clicked = False
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
 
         # draw button
         screen.blit(self.image, self.rect)
+
         return action
-        """
 
 
-def draw_grid():
-    for line in range(0, 20):
-        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
-
-class World:
+class World():
     def __init__(self, data):
-
         self.tile_list = []
 
-        # load image of dirt
-        dit_img = pygame.image.load("gamegraphics/dirt.png")
-        dirt_img = pygame.transform.scale(dit_img, (50,50))
-
-        # load image of grass
-        gras_img = pygame.image.load("gamegraphics/grass.png")
-        grass_img = pygame.transform.scale(gras_img, (50, 50))
+        # load images
+        dirt_img = pygame.image.load('img/dirt.png')
+        grass_img = pygame.image.load('img/grass.png')
 
         row_count = 0
         for row in data:
@@ -89,29 +81,32 @@ class World:
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
-            col_count += 1
-        row_count += 1
+
+                col_count += 1
+            row_count += 1
 
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
+            pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
 
 
 world_data = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0],
-    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0],
-    [0, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 0],
-    [0, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
     [0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1],
@@ -119,35 +114,34 @@ world_data = [
     [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+
+
 world = World(world_data)
 
-# buttons
-start_button = Buttons(screen_width // 2 - 350, screen_height // 2, start_img)
-exit_button = Buttons(screen_width // 2 + 150, screen_height // 2, exit_img)
+# create buttons
+start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 150, screen_height // 2, exit_img)
 
-# to keep the game running
 run = True
 while run:
-    # put images on the game screen
+
+    clock.tick(fps)
+
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
-    if main_menu:
-        # if exit_button.draw():
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+    if main_menu == True:
+        if exit_button.draw():
             run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_s:
-                main_menu = False
+        if start_button.draw():
+            main_menu = False
     else:
         world.draw()
-        draw_grid()
-        print(world.tile_list)
 
-        # a false statement to allow to quit the run
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        run = False
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     pygame.display.update()
 
